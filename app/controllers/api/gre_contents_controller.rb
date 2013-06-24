b133d87f4d98ff23e37ea6b563ee9d1354b48c8c
Amit Shrestha <shrestha_canada07@yahoo.com>
2013-06-24 11:20:25 +0545

    authentication fixes.

--- app/controllers/api/gre_contents_controller.rb
+++ app/controllers/api/gre_contents_controller.rb
@@ -1,5 +1,5 @@
 class Api::GreContentsController < ApplicationController
-  skip_before_filter :protect_from_forgery
+  skip_before_filter :protect_from_forgery, :authenticate
   def index
     @gre_contents = GreContent.all
     render json: @gre_contents
